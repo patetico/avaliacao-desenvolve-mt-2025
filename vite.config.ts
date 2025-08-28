37299bf8f8d9719d8b89b95eb88739d8d0d4927e
patetico <pat@pateti.com>
2025-08-28 14:58:39 -0400

    ativa https para uso local

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
+import ssl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tspaths from "vite-tsconfig-paths";
@@ -7,6 +8,7 @@ import tspaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    ssl(),
     tspaths(),
     tailwindcss(),
     react(),
