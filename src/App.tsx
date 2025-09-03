acf56743366e5c91dbed39ac3d844b692bfb2636
patetico <pat@pateti.com>
2025-09-03 03:46:51 -0400

    organização da estrutura da página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,16 @@ function App() {
 
   return (
     <ThemeProvider>
-      <SearchForm />
+      {/* TODO: <PageHeader /> */}
+      <div className="flex flex-col lg:flex-row">
+        <aside className="md:border-r px-8 py-4">
+          <SearchForm />
+        </aside>
+        <main className="px-16 py-8">
+          {/* TODO: <DesaparecidosList /> */}
+        </main>
+      </div>
+      {/* TODO: <PageFooter /> */}
     </ThemeProvider>
   )
 }
