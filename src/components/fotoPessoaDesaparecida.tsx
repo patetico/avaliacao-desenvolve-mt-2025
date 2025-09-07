8fd2415b30d8028bf455d8679f27319089407482
patetico <pat@pateti.com>
2025-09-07 17:55:15 -0400

    opção de alinhamento no componente FotoPessoaDesaparecida

--- src/components/fotoPessoaDesaparecida.tsx
+++ src/components/fotoPessoaDesaparecida.tsx
@@ -1,10 +1,30 @@
-import MissingPerson from '~/components/icons/MissingPerson.tsx';
+import MissingPerson from '~/components/icons/MissingPerson';
+import { cn } from '~/lib/shadcn';
 
 
-function FotoPessoaDesaparecida({ url }: { url?: string }) {
+interface FotoPessoaDesaparecidaProps {
+  url?: string;
+  align?: 'center' | 'top';
+}
+
+
+function FotoPessoaDesaparecida({ url, align }: FotoPessoaDesaparecidaProps) {
+  const baseCls = 'size-full';
+  const alignTop = align === 'top'; // center por padrão
+
   return url
-    ? (<img src={url} alt="Foto da pessoa desaparecida" className="object-contain size-full" />)
-    : (<MissingPerson aria-label="Ilustração de pessoa desaparecida sem foto" className="size-full" />);
+    ? (
+      <img
+        src={url}
+        alt="Foto da pessoa desaparecida"
+        className={cn(baseCls, 'object-contain', alignTop ? 'object-top' : 'object-center')}
+      />)
+    : (
+      <MissingPerson
+        aria-label="Ilustração de pessoa desaparecida sem foto"
+        className={baseCls}
+        preserveAspectRatio={alignTop ? 'xMidYMin meet' : 'xMidYMid meet'}
+      />);
 }
 
 export default FotoPessoaDesaparecida;
